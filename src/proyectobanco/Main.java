a9c3ab5e2dc384a13d1bbe81311ae2aea088f27f
jaine <jaine@JASA>
2025-07-19 17:56:39 -0600

    git commit -m "Commit inicial: Primero cree la clase Main y despues importe las librerias

---
+++ src/proyectobanco/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectobanco;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author jaine
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
